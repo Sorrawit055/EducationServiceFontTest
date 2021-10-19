4df5aeaf45d62458044f27ad8b8d4cc2b307ac02
Sorrawit Eaknarongpan <614259055@webmail.npru.ac.th>
2021-10-19 13:12:41 +0700

    Update

--- src/components/ViewTeacher/ViewDashboardTeacher.js
+++ src/components/ViewTeacher/ViewDashboardTeacher.js
@@ -39,7 +39,7 @@ const ViewDashboardTeacher = (props) => {
       name: 'หลักสูตร',
       href: '/Teacher/degreeall',
       description:
-      '',
+        '',
       icon: AcademicCapIcon,
     },
   ]
@@ -105,7 +105,7 @@ const ViewDashboardTeacher = (props) => {
                       <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
                         <feature.icon className="h-6 w-6" aria-hidden="true" />
                       </div>
-                      <p className="ml-16 text-lg leading-6 font-medium text-gray-900">{feature.name}</p>
+                      <p className="ml-16 text-lg leading-6 font-medium text-gray-900" id={feature.name} name={feature.name}>{feature.name}</p>
                     </dt>
                     <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
                   </div>
@@ -122,7 +122,7 @@ const ViewDashboardTeacher = (props) => {
                       <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
                         <feature.icon className="h-6 w-6" aria-hidden="true" />
                       </div>
-                      <p className="ml-16 text-lg leading-6 font-medium text-gray-900">{feature.name}</p>
+                      <p className="ml-16 text-lg leading-6 font-medium text-gray-900" id={feature.name} name={feature.name} >{feature.name}</p>
                     </dt>
                     <dd className="mt-2 ml-16 text-base text-gray-500">{feature.description}</dd>
                   </div>
